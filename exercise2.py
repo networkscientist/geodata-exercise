baffa579809836a81d74fde042c0a169c9c92e06
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2021-03-12 11:17:05 +0100

    feature: Introduce a function to do the range check

--- exercise2.py
+++ exercise2.py
@@ -1,34 +1,42 @@
 import random
 
 
-def random_check(rand, guess):  # Takes the luckyNumber and the user's guess as an argument. Checks if the luckyNumber
-    # has been guessed correctly. Print hints for the next guess and return True or False.
-    """Takes the luckyNumber and the user's guess as an argument. Checks if the luckyNumber has been guessed
-    correctly. Print hints for the next guess and return True or False. """
-    if guess in range(0, 101):
-        if rand > guess:
+def random_check(luckynumber, guess):  # Takes the luckyNumber and the user's guess as an argument. Checks if the
+    # luckyNumber has been guessed correctly. Print hints for the next guess and return True or False.
+    """ Check if the luckynumber the same as the guessed number."""
+    if range_check(guess):
+        if luckynumber > guess:
             print("Go a little higher...")
             return False
-        elif rand < guess:
+        elif luckynumber < guess:
             print("Too high, go again...")
             return False
         else:
             print("Yay, You guessed correctly!")
             return True
     else:
-        print("Come on, enter a number within the specified range 0-100!")
         return False
 
 
 def get_guess():  # This function gets a user's guess for the lucky number and returns it as an integer.
     """This function gets a user's guess for the lucky number and returns it as an integer."""
-    user_guess = input("Please guess a number between 0 and 100: ")
+    user_guess = input()
     return int(user_guess)
+
+
+def range_check(num):  # Checking if the passed number is within the range of allowed values.
+    """Check if passed number is in allowed range."""
+    if num in range(0, 101):
+        return True
+    else:
+        print("Come on, enter a number within the specified range 0-100!")
+        return False
 
 
 # The magic happens: Draw a luckyNumber between 0 and 100
 luckyNumber = random.randint(0, 100)
 success = False
 # The loop goes on as long as the user has not guessed correctly, the it stops.
+print("Let's begin... Please guess a number between 0 and 100:")
 while not success:
     success = random_check(luckyNumber, get_guess())
