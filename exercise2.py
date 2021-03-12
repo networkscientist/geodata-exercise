46e46bcedb69909b058b2d392502edee9994645c
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2021-03-12 10:02:29 +0100

    Cleaned up code and expanded the comments.

--- exercise2.py
+++ exercise2.py
@@ -1,8 +1,10 @@
 import random
 
 
-def random_check(rand):
-    guess = get_guess()
+def random_check(rand, guess):  # Takes the luckyNumber and the user's guess as an argument. Checks if the luckyNumber
+    # has been guessed correctly. Print hints for the next guess and return True or False.
+    """Takes the luckyNumber and the user's guess as an argument. Checks if the luckyNumber has been guessed
+    correctly. Print hints for the next guess and return True or False. """
     if rand > guess:
         print("Go a little higher...")
         return False
@@ -14,12 +16,15 @@ def random_check(rand):
         return True
 
 
-def get_guess():
+def get_guess():  # This function gets a user's guess for the lucky number and returns it as an integer.
+    """This function gets a user's guess for the lucky number and returns it as an integer."""
     user_guess = input("Please guess a number between 0 and 100: ")
     return int(user_guess)
 
 
+# The magic happens: Draw a luckyNumber between 0 and 100
 luckyNumber = random.randint(0, 100)
 success = False
+# The loop goes on as long as the user has not guessed correctly, the it stops.
 while not success:
-    success = random_check(luckyNumber)
+    success = random_check(luckyNumber, get_guess())
