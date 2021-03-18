5743724a979f7b85977795f3cb4f53a8ab1cea38
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2021-03-18 20:12:01 +0100

    refactor: Code cleanup & commenting

--- exercise3.py
+++ exercise3.py
@@ -8,12 +8,11 @@ def file_reader(path):
 
 
 input_file = "data/precip_data.txt"
+# call the file_reader function with the input_file path and store the returned data as pd
 pd = file_reader(input_file)
+# Calculate three statistical value and store the as a dictionary
 statistics = {'Min': pd.min(), 'Max': pd.max(), 'Mean': pd.mean()}
 print("Precipitation Data:\n")
+# Go through the statistics dictionary and print the values in an ordered fashion
 for x, y in statistics.items():
     print('{0:4}:   {1:4.1f} mm'.format(x, y))
-# • The script: it should call the function and display to the user:
-# • The min precipitation
-# • The max precipitation
-# • The mean precipitation
